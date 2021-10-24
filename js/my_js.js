bc453935739f0726efdb4885f1e7fb025b73984b
Hà Mạnh Tín <hatin7080@gmail.com>
2021-10-24 20:56:38 +0700

    a

--- js/my_js.js
+++ js/my_js.js
@@ -2,7 +2,7 @@ const year = new Date().getFullYear();
 const fourthOfJuly = new Date(year, 10, 20).getTime();
 const fourthOfJulyNextYear = new Date(year + 1, 10, 20).getTime();
 const month = new Date().getMonth();
-const countDownDate = new Date("Oct 24, 2021 10:36:00").getTime();
+const countDownDate = new Date("Nov 7, 2021 00:00:00").getTime();
 const countDownDateNextY = new Date("Nov 7, 2022 00:00:00").getTime();
 // countdown	1637254800000
 let timer = setInterval(function() {
